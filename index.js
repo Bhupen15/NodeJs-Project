780cae2b7a470e55a5fef697fcee253f10d09ae1
Bhupendra Choudhary <80590016+Bhupen15@users.noreply.github.com>
2023-03-17 12:50:47 +0530

    Nodejs code

---
+++ index.js
@@ -0,0 +1,19 @@
+// const http = require('http');
+
+// // http.createServer((req, res)=>{
+// //     res.write("<h1>Hello this is chhapri</h1>");
+// //     res.end();
+// // }).listen(4500);
+
+// function datacontrol(req, res) {
+//     res.write("<h1>Hello this is Bhupendra </h1>");
+//     res.end();
+
+// }
+
+// http.createServer(datacontrol).listen(4500);
+
+
+const colors = require('colors')
+
+console.log("I'm colorful".red)
